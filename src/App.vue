f01db03fc51bd97f9f2c108a1a86fd6bd3267d22
Muhamad Azis <88225954+muhamadAzis32@users.noreply.github.com>
2025-07-14 13:37:05 +0700

    ✨ feat: Membuat Counter App Versi Options Api

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
-<script setup>
+<script >
+export default {
+  data() {
+    return {
+      count: 1,
+    };
+  },
 
-import { ref } from 'vue';
-const count = ref(0)
+  methods: {
+    increase() {
+      this.count++;
+    },
 
+    decrease() {
+      this.count--;
+    },
+  },
+};
 </script>
 
 <template>
@@ -10,24 +23,23 @@ const count = ref(0)
     <h1>Counter</h1>
     <span class="" id="value">{{ count }}</span>
     <div>
-      <button @click="count--" id="decrease">-</button>
-      <button @click="count++" id="increase">+</button>
+      <button @click="decrease" id="decrease">-</button>
+      <button @click="increase" id="increase">+</button>
     </div>
   </div>
 </template>
 
 // hanya digunkan pada komponen ini saja
 <style scoped>
-h1{
+h1 {
   font-size: 50px;
   color: red;
 }
 
-button{
+button {
   height: 30px;
   width: 50px;
   margin-right: 5px;
   margin-top: 10px;
 }
-
 </style>
